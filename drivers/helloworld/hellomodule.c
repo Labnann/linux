d9ee573e7a107bcff7503916b0fb9ed220d156e1
Khalid Masum <khalid.masum.92@gmail.com>
2022-05-15 21:38:33 +0600

    pass string arg into hellomodule and print
    After this change we will be pass a string into hellomodule using the
    mystring global variable which will be printed upon loading the module.
    
    Signed-off-by: Khalid Masum <khalid.masum.92@gmail.com>

--- drivers/helloworld/hellomodule.c
+++ drivers/helloworld/hellomodule.c
@@ -1,3 +1,4 @@
+
 // SPDX-License-Identifier: GPL-2.0-or-later
 /*
  *      hellomodule.c  -- Hello World module
@@ -7,27 +8,40 @@
 #include <linux/kernel.h>
 #include <linux/init.h>
 
-
 #define DRIVER_AUTHOR		"Khalid Masum" \
 				"<khalid.masum.92@gmail.com>"
 #define DRIVER_DESC		"Hello World module"
 
+/*global variables that are to be set from
+*command line as args
+*/
 
+char *mystring = "to_be_set";
+
+/*
+* set up cmmand line arguement mechanism for
+* global variables
+*/
+
+module_param(mystring, charp, 0000);
 
 static int __init hello_world_init(void)
 {
-	printk("mymodule1 Hello world!\n");
+	printk(KERN_INFO "mymodule1 Hello world!\n");
+	printk(KERN_INFO "%s\n", mystring);
+	printk(KERN_INFO "Done Printing");
+	
 	return 0;
 }
 
 static void __exit hello_world_cleanup(void)
 {
-	printk("mymodule1e Clean up Hello World.\n");
+	printk(KERN_INFO "mymodule1e Clean up Hello World.\n");
 }
+
 
 module_init(hello_world_init);
 module_exit(hello_world_cleanup);
 MODULE_AUTHOR(DRIVER_AUTHOR);
 MODULE_DESCRIPTION(DRIVER_DESC);
 MODULE_LICENSE("GPL");
-
